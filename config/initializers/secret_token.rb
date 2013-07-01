99bc033beede64f48f9fe62a16828e177911fcd8
Moresh <moreshk@gmail.com>
2013-06-30 22:07:52 -0500

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoreshApp::Application.config.secret_token = '6f3c5d2f11077e03e4d05ff8ea7bfb755a0de7b6308d317a6d4bc68e3ef6ef0514d8dcbc383eee75d85f3e0935fffdafe6c8396e0b7ebe2ed8a85037f9354084'
